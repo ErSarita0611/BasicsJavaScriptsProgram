d2ed31893b22d9db6eb57d1537b2e04dd41f34b7
@SaritaKuamri0611 <151984965+ErSarita0611@users.noreply.github.com>
2023-12-11 11:12:35 +0000

    datatypesummery

---
+++ JavaSriptbasicProgramAndNotes/05_datatype-sammery.js
@@ -0,0 +1,39 @@
+// Primitive 
+
+// 7 Types : String , Number , Boolean , Null, Undefined, Symbol, Bigint 
+
+const score = 100
+const scoreValue = false
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 65562326595666444n
+
+
+// REference (Non Primitive Datatype)
+
+// Array, Objects, Functions
+
+
+
+const heros = ["shaktiman","naagraj","dogo"]
+let myobj = {
+    name : "sarita",
+    age : 22,
+
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof heros);
+
+
