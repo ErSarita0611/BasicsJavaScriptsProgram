f2de27f3ef491c631b62f2fdc608af0f1ed010de
@SaritaKuamri0611 <151984965+ErSarita0611@users.noreply.github.com>
2023-12-12 09:42:09 +0000

    changesfunction

--- 02_basicsFunction/01_function.js
+++ 02_basicsFunction/01_function.js
@@ -24,19 +24,49 @@ function saymyname(){
     console.log("Result", result);
     
     
-    function loginUserMassage(usename){
-        if (username === undefined){ 
+    function loginUserMassage(username = "sari"){
+        if (!username){ 
         console.log("Please Enter a Username");
         return 
             
         }
-        return '${username} just logged in'
+        return `${username} just logged in`
     
     }
      
     // console.log(loginUserMassage("sarita"));
-    console.log(loginUserMassage());
-    
+    console.log(loginUserMassage("saru"));
+
+
+    function calculateCartPrice (val1, val2, ...num1){    //... rest operator and sperate operator
+        return num1
+    }
+     console.log(calculateCartPrice(200, 400, 600));
+
+     const user = {
+        username: "sarita",
+        price: 155
+     }
+
+     function handleObject(anyobject){
+        console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+     }
+
+     handleObject(user)
+
+     handleObject({
+        username: "saru",
+        price: 522
+     })
+
+
+     const mynewArray = [200, 400, 500, 600]
+     function returnSecondValue(getArray){
+        return getArray[1]
+     }
+
+     console.log(returnSecondValue(mynewArray));
+     console.log(returnSecondValue([200, 500, 400, 600]));
     
     
     
