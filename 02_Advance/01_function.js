a5045b29712a80df36d46d2ae5dae3f716bab7a5
@SaritaKuamri0611 <151984965+ErSarita0611@users.noreply.github.com>
2023-12-12 09:01:30 +0000

    function

---
+++ 02_Advance/01_function.js
@@ -0,0 +1,42 @@
+function saymyname(){
+console.log("S");
+console.log("A");
+console.log("R");
+console.log("I");
+console.log("T");
+console.log("A");
+
+}
+// saymyname()
+
+function addTwoNumbers(number1, number2){
+  console.log(number1 + number2);
+}
+
+function addTwoNumbers2(number1, number2){
+
+    let result = number1 + number2
+    return result
+    console.log(number1 + number2);
+  }
+
+const result = addTwoNumbers2(3, 5)
+console.log("Result", result);
+
+
+function loginUserMassage(usename){
+    if (username === undefined){ 
+    console.log("Please Enter a Username");
+    return 
+        
+    }
+    return '${username} just logged in'
+
+}
+ 
+// console.log(loginUserMassage("sarita"));
+console.log(loginUserMassage());
+
+
+
+
