c6d3195a850bed99b20ffeeafcba3fec577ad632
@SaritaKuamri0611 <151984965+ErSarita0611@users.noreply.github.com>
2023-12-12 11:41:07 +0000

    arrow

---
+++ 02_Advance/03_arrow.js
@@ -0,0 +1,10 @@
+const user = {
+    username: "sarita",
+    price: 999
+
+    // welcomeMassage: function(){
+    //     console.log(`${this.username} , welcome to website`);
+    // }
+    
+}
+user.welcomeMassage()
