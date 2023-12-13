b0580c84529b5fcbdb0da9e3bfe7aa226de7ad3a
@SaritaKuamri0611 <151984965+ErSarita0611@users.noreply.github.com>
2023-12-13 06:14:54 +0000

    iife

---
+++ 02_Advance/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // named iife
+    console.log(`DB CONNECTED`);
+})();
+
+( function aurcode() {
+    console.log(`DB CONNECTED TWO`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('sarita')
