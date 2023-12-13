9bb8ccb24943475a13c77064936dd3fd65473084
@SaritaKuamri0611 <151984965+ErSarita0611@users.noreply.github.com>
2023-12-13 05:52:25 +0000

    changes

--- 02_Advance/03_arrow.js
+++ 02_Advance/03_arrow.js
@@ -10,9 +10,41 @@ const user = {
 // user.welcomeMassage()
 
 
-function chai(){
-    let username = "sarita"
-    console.log(this);
+// function chai(){
+//     let username = "sarita"
+//     console.log(this);
 
+// }
+// chai()
+
+const chai = function(){
+    let username = "sarita"
+    console.log(this.username);
 }
 chai()
+
+const chai1 = () => {
+    let username = "sarita"
+    console.log(this);
+}
+chai1()
+
+// Arrow Function
+
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3, 4));
+
+// Implicit Arrow
+
+const addTwos = (num1, num2) =>  ( num1 + num2 )
+console.log(addTwos(3, 4));
+
+
+const addTwo2 = (num1, num2) =>  ( {username: "sarita"} )
+console.log(addTwo2(3, 4));
+
+// const muArray = [2, 5, 6, 8]
+// muArray.forEach()
