a5d1768a380fff30d8e3b1a678a8fa222814bc8d
@SaritaKuamri0611 <151984965+ErSarita0611@users.noreply.github.com>
2023-12-13 08:37:37 +0000

    loops

---
+++ 04_iterations_loops/02_while_and_dowhile_loop.js
@@ -0,0 +1,24 @@
+
+let index = 0
+
+while (index <= 10) {
+    console.log(`Value of Index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 1
+while (arr < myArray.length) {
+//     console.log(`Value is: ${myArray[arr]}`);
+//     arr = aar + 1
+    
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
